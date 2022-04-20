5a1047ccdb3718c2e761ae7a24b6248e552abac3
Dhafinur27 <maulanaandre2701@gmail.com>
2022-04-20 07:20:03 +0700

    python

---
+++ test.py
@@ -0,0 +1,13 @@
+# list sebagai iterable
+gorengan = ['bakwan','cireng','tahu isi','tempe goreng','ubi goreng']
+
+for g in gorengan:
+    print(g)
+    print(len(g))
+
+# string sebagai iterable
+
+bakwan = 'bakwan'
+
+for i in bakwan:
+    print(i)
